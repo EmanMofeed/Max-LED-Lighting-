07a66594c5a9cac0eb0441e801256c29634196dd
EmanMofeed <86316644+EmanMofeed@users.noreply.github.com>
2022-03-08 22:36:59 +0200

    Add files via upload

---
+++ LedsBoard.java
@@ -0,0 +1,28 @@
+package sample;
+
+import java.util.ArrayList;
+/*
+  two arrayList
+ */
+
+public class LedsBoard {
+    private ArrayList<Led> leds;
+    private ArrayList<Power> power;
+
+
+    public ArrayList<Led> getLeds() {
+        return leds;
+    }
+
+    public void setLeds(ArrayList<Led> leds) {
+        this.leds = leds;
+    }
+
+    public ArrayList<Power> getPower() {
+        return power;
+    }
+
+    public void setPower(ArrayList<Power> power) {
+        this.power = power;
+    }
+}
